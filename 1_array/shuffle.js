8f2372fdeb5d8bd8a2bce57fa53942b874ab191d
Shahll <130846011+Shahll@users.noreply.github.com>
2023-07-14 23:11:37 +0200

    Update shuffle.js

--- 1_array/shuffle.js
+++ 1_array/shuffle.js
@@ -1,14 +1,14 @@
 function random(min, max){
-    return Math.floor(Math.random() * (max - min)) + min
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
 for (let i = 0; i < 100; i++){
     let index = random(0, arr.length);
-    let temp = arr[0]
-    arr[0] = arr[index]
-    arr[index] = temp
+    let temp = arr[0];
+    arr[0] = arr[index];
+    arr[index] = temp;
 
 }
 
