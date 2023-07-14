149cf7c6a7bcc1e3bb5e68c1e2cc69a0c1d03fbd
Shahll <130846011+Shahll@users.noreply.github.com>
2023-07-14 22:41:00 +0200

    Add files via upload

---
+++ 1_array/delete_all.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 3];
+let target = 3;
+
+for (let i = 0; i < arr.length; i ++){
+    if (arr[i] == target){
+        arr.splice(i, 1)
+    }
+}
+
+for (let i = 0; i < arr.length; i ++){
+    console.log(arr[i])
+}
