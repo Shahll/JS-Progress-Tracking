5f7f0bf86378582b85eb74f5045e43506e513dd6
Shahll <130846011+Shahll@users.noreply.github.com>
2023-07-14 22:17:26 +0200

    Add files via upload

---
+++ 1_array/del_first_last_mid.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5, 6, 7]
+let mid;
+
+arr.splice(0, 1)
+arr.splice(arr.length -1, 1)
+mid = arr.length / 2
+arr.splice(mid, 1)
+
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
