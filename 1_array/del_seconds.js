a3a01814157ef41195c56d0e6058538bd123da8a
Shahll <130846011+Shahll@users.noreply.github.com>
2023-07-14 23:12:55 +0200

    Update del_seconds.js

--- 1_array/del_seconds.js
+++ 1_array/del_seconds.js
@@ -1,6 +1,6 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-arr = arr.filter((num) => num % 2 == 1)
+arr = arr.filter((num) => num % 2 == 1);
 
 for (let i = 0; i < arr.length; i ++){
-    console.log(arr[i])
+    console.log(arr[i]);
 }
